494aee88fbe85eb020eea854f00a0c97f30f88d8
iWe <16604027+iWECon@users.noreply.github.com>
2022-03-29 11:30:17 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/iWECon/Pager", from: "1.0.0"),
-        .package(url: "https://github.com/iWECon/Segmenter", from: "1.0.0")
+        .package(url: "https://github.com/iWECon/Segmenter", from: "2.0.0")
     ],
     targets: [
         .target(name: "SegmentedController", dependencies: ["Pager", "Segmenter"]),
