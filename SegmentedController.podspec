6ab7e1c44a7612022a97fe22e62e1c090ba73c59
iWw <i.1214@yahoo.com>
2023-07-24 16:15:47 +0800

    Bump version to 2.1.2

--- SegmentedController.podspec
+++ SegmentedController.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
     s.name = 'SegmentedController'
-    s.version = '2.1.1'
+    s.version = '2.1.2'
     s.license = { :type => 'MIT' }
     s.homepage = 'https://github.com/iWECon/SegmentedController'
     s.authors = 'iWw'
--- Sources/SegmentedController/SegmentedControllerTransitionDelegate.swift
+++ Sources/SegmentedController/SegmentedControllerTransitionDelegate.swift
@@ -5,13 +5,13 @@ import YTPageController
 // see: YTPageController.h:214  `YTPageControllerDelegate`
 public protocol SegmentedControllerTransitionDelegate: AnyObject {
     
-    func segmentedController(_ controller: SegmentedController, pageController: PageController, willStartTransition: PageTransitionContext)
-    func segmentedController(_ controller: SegmentedController, pageController: PageController, didUpdateTransition: PageTransitionContext)
-    func segmentedController(_ controller: SegmentedController, pageController: PageController, didEndTransition: PageTransitionContext)
+    func segmentedController(_ controller: SegmentedController, pageController: PageController, willStartTransition context: PageTransitionContext)
+    func segmentedController(_ controller: SegmentedController, pageController: PageController, didUpdateTransition context: PageTransitionContext)
+    func segmentedController(_ controller: SegmentedController, pageController: PageController, didEndTransition context: PageTransitionContext)
 }
 
 extension SegmentedControllerTransitionDelegate {
     
-    public func segmentedController(_ controller: SegmentedController, pageController: PageController, didUpdateTransition: PageTransitionContext) { }
-    public func segmentedController(_ controller: SegmentedController, pageController: PageController, didEndTransition: PageTransitionContext) { }
+    public func segmentedController(_ controller: SegmentedController, pageController: PageController, didUpdateTransition context: PageTransitionContext) { }
+    public func segmentedController(_ controller: SegmentedController, pageController: PageController, didEndTransition context: PageTransitionContext) { }
 }
